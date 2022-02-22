d8ced0710db696aabc75b110ded0d07e49f63667
stecaulab <cauzzistefanoicloud.com>
2022-02-22 23:40:32 +0100

    add databasa table

---
+++ database/migrations/2022_02_22_220638_create_archives_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArchivesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('archives', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('model_id');
+            $table->string('model_type','150');
+            $table->string('route','150');
+            $table->string('extention','10');
+            $table->integer('size');
+            $table->boolean('local')->defaul();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('archives');
+    }
+}
